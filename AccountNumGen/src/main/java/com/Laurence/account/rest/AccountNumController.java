549398982b6068e11714030ba531af0aff348e05
Laurence Garcia <60017111+lmcg05@users.noreply.github.com>
2020-04-24 14:34:13 +0100

    Update AccountNumController.java

--- AccountNumGen/src/main/java/com/Laurence/account/rest/AccountNumController.java
+++ AccountNumGen/src/main/java/com/Laurence/account/rest/AccountNumController.java
@@ -1,10 +1,10 @@
-package com.tyler.account.rest;
+package com.laurence.account.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tyler.account.service.AccountNumGenService;
+import com.laurence.account.service.AccountNumGenService;
 
 @RestController
 @RequestMapping("/account")
